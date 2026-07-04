8495e9986c50b3f3c161d523a1e66d641a00b2c7
almoreno01 <morenoyester2@gmail.com>
2026-07-04 16:34:17 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 // Pega la Project URL y la Publishable key sb_publishable_...
 // NO uses sb_secret_ ni service_role.
 window.NEXUS_SUPABASE_CONFIG = {
-  url: "https://xvsxwypcqyfdiwmthpjv.supabase.co/rest/v1/",
+  url: "https://xvsxwypcqyfdiwmthpjv.supabase.co",
   publishableKey: "sb_publishable_466U-x5n2aO8EY-KKv3loQ_5hFO4Lv4"
 };
 window.NEXUS_CLOUD_DOC = "equipo_principal";
